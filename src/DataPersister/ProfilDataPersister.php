534f37aaa2401409573383d75e0c6660e9199eab
buzzpop <rassoulbuzzpop@gmail.com>
2020-11-20 15:29:31 +0000

    rassoul

---
+++ src/DataPersister/ProfilDataPersister.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\DataPersister;
+
+
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use App\Entity\Profil;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProfilDataPersister implements ContextAwareDataPersisterInterface
+{
+    private $entityManager;
+    public  function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager= $entityManager;
+
+    }
+
+
+    public function supports($data, array $context = []): bool
+    {
+        // TODO: Implement supports() method.
+        return $data instanceof Profil;
+    }
+
+    public function persist($data, array $context = [])
+    {
+        // TODO: Implement persist() method.
+    }
+
+    public function remove($data, array $context = [])
+    {
+        // TODO: Implement remove() method.
+        $archive=$data->setArchivage(true);
+        $this->entityManager->persist($archive);
+        $users= $data->getUsers();
+      foreach ($users as $user){
+         $archiveUser =$user->setStatut(true);
+          $this->entityManager->persist($archiveUser);
+      }
+            $this->entityManager->flush();
+
+    }
+}
